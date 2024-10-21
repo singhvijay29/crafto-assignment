f60e73e11e5f2ddb7155af54c3939759570adeb9
Vijay Kumar Singh <86338308+singhvijay29@users.noreply.github.com>
2024-10-21 18:31:42 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const router = useRouter();
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-6">
         <a className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             crafto
